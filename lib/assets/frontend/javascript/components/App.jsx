e0acc2d16a3f43623e4a60642048ff53e65c9e95
Konstantin S. Kazarin <kazarin.konstantin@gmail.com>
2017-01-02 18:26:46 +1000

    Clean

--- lib/assets/frontend/javascript/components/App.jsx
+++ lib/assets/frontend/javascript/components/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-//import Search from '../containers/Search';
-//import Prefixes from '../containers/Prefixes';
-//import NextPage from '../containers/NextPage';
-//import PreviousPage from '../components/PreviousPageLink';
 import PaginationContainer from '../containers/PaginationContainer';
 import PrefixesContainer from '../containers/PrefixesContainer';
 import SearchContainer from '../containers/SearchContainer';
